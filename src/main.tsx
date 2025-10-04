d975441a4ea26a299f63d58ef47807e82ba59cf5
MiGaNg <miguelangelyv1@gmail.com>
2025-10-03 20:45:59 -0500

    Inicio de aplicación - HooksApp

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { HooksApp } from './HooksApp'
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <HooksApp />
+  </StrictMode>,
+)
