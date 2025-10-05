d83c97986eb308938e7a5c19759d098ce223c44e
MiGaNg <miguelangelyv1@gmail.com>
2025-10-04 22:31:22 -0500

    Implementación de TailwindCSS y estilos

--- src/HooksApp.tsx
+++ src/HooksApp.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+
 
 export const HooksApp = () => {
     return (
-        <div>HooksApp</div>
+        <div className="bg-gradient">
+            <h1 className="text-3xl font-bold underline">    Hello world!  </h1>
+        </div>
     )
 }
