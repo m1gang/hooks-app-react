8d301caac6b055c212bd961b57101260019fcde1
MiGaNg <miguelangelyv1@gmail.com>
2025-10-06 18:52:49 -0500

    useRef - Valora que no dispara re-render usando select en el input

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 import './index.css'
+import { FocusScreen } from './04-useRef/FocusScreen'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
-import { PokemonPage } from './03-examples/PokemonPage'
+// import { PokemonPage } from './03-examples/PokemonPage'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
-    <PokemonPage />
+    {/* <PokemonPage /> */}
+    <FocusScreen />
   </StrictMode>,
 )
