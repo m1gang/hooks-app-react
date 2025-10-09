0418de7b0d8fd5c070c7b70dbc43c15df3531c32
MiGaNg <miguelangelyv1@gmail.com>
2025-10-08 20:40:06 -0500

    Validadores de objetos - Zod para datos del localStorage

--- src/05-useReducer/reducer/tasksReducer.ts
+++ src/05-useReducer/reducer/tasksReducer.ts
@@ -1,3 +1,5 @@
+import * as z from "zod";
+
 interface Todo {
     id: number;
     text: string;
@@ -8,6 +10,20 @@ export type TaskAction =
     | { type: 'ADD_TODO', payload: string }
     | { type: 'TOGGLE_TODO', payload: number }
     | { type: 'DELETE_TODO', payload: number }
+
+
+const TodoSchema = z.object({
+    id: z.number(),
+    text: z.string(),
+    completed: z.boolean(),
+})
+
+const TaskStateSchema = z.object({
+    todos: z.array(TodoSchema),
+    length: z.number(),
+    completed: z.number(),
+    pending: z.number(),
+})
 
 export const getTasksInitialState = (): TaskState => {
     const localStorageState = localStorage.getItem('tasks-state');
@@ -20,8 +36,21 @@ export const getTasksInitialState = (): TaskState => {
             length: 0
         }
     };
+
+    // Validar mediante Zod
+    const result = TaskStateSchema.safeParse(JSON.parse(localStorageState));
+    if (result.error) {
+        console.log(result.error);
+        return {
+            todos: [],
+            completed: 0,
+            pending: 0,
+            length: 0
+        }
+    }
+
     // ! Cuidado, porque el objeto puede haber sido manipulado
-    return JSON.parse(localStorageState);
+    return result.data;
 }
 
 interface TaskState {
