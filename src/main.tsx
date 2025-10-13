d64083b4cb4d3d1da47bcd7fa6a6aa9da54db551
MiGaNg <miguelangelyv1@gmail.com>
2025-10-13 17:34:39 -0500

    Tarea y resolución de app palabras resueltas

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 import './index.css'
-import { TasksApp } from './05-useReducer/TaskApp'
+import { ScrambleWords } from './05-useReducer/ScrambleWords'
+// import { TasksApp } from './05-useReducer/TaskApp'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 // import { PokemonPage } from './03-examples/PokemonPage'
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
     {/* <FocusScreen /> */}
-    <TasksApp />
+    {/* <TasksApp /> */}
+    <ScrambleWords />
   </StrictMode>,
 )
