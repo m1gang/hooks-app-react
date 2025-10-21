374797fe2b072cb0a96ef959ff789e04912f9e8f
MiGaNg <miguelangelyv1@gmail.com>
2025-10-21 10:59:48 -0500

    Contexto de usuario - Información global

--- src/09-useContext/context/UserContext.tsx
+++ src/09-useContext/context/UserContext.tsx
@@ -1,14 +1,45 @@
-import { type PropsWithChildren } from "react"
+import { createContext, useState, type PropsWithChildren } from "react"
+import type { User } from "../data/user-mock.data";
 
 // interface UserContextProps {
 //     children: React.ReactNode
 // }
+type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
+
+interface UserContextProps {
+    //state
+    authStatus: AuthStatus;
+    user: User | null;
+
+    //methods
+    login: (userId: number) => boolean;
+    logout: () => void;
+}
+
+export const UserContext = createContext({} as UserContextProps);
 
 export const UserContextProvider = ({ children }: PropsWithChildren) => {
+    const [authStatus, setAuthStatus] = useState<AuthStatus>('checking');
+    const [user, setUser] = useState(null);
+    const handleLogin = (userId: number) => {
+        console.log({ userId });
+        return true;
+    }
+    const handleLogout = () => {
+        console.log('logout');
 
+    }
     return (
         <>
-            {children}
+            <UserContext value={{
+                authStatus: authStatus,
+                user,
+                login: handleLogin,
+                logout: handleLogout,
+
+            }}>
+                {children}
+            </UserContext>
         </>
     )
 }
