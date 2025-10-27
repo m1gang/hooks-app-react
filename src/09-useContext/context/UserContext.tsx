40c5856803ad7a86f4ae66bcf80a910997617d6b
MiGaNg <miguelangelyv1@gmail.com>
2025-10-27 09:47:23 -0500

    Consumir el contexto - use y useContext

--- src/09-useContext/context/UserContext.tsx
+++ src/09-useContext/context/UserContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, useState, type PropsWithChildren } from "react"
-import type { User } from "../data/user-mock.data";
+import { users, type User } from "../data/user-mock.data";
+import { set } from "zod";
 
 // interface UserContextProps {
 //     children: React.ReactNode
@@ -20,15 +21,27 @@ export const UserContext = createContext({} as UserContextProps);
 
 export const UserContextProvider = ({ children }: PropsWithChildren) => {
     const [authStatus, setAuthStatus] = useState<AuthStatus>('checking');
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState<User | null>(null);
+
     const handleLogin = (userId: number) => {
-        console.log({ userId });
+        const user = users.find(user => user.id === userId);
+        if (!user) {
+            console.log(`Usuario no encontrado ${userId}`);
+            setUser(null);
+            setAuthStatus('not-authenticated');
+            return false;
+        }
+        setUser(user);
+        setAuthStatus('authenticated');
         return true;
     }
+
     const handleLogout = () => {
         console.log('logout');
-
+        setUser(null);
+        setAuthStatus('not-authenticated');
     }
+
     return (
         <>
             <UserContext value={{
--- src/09-useContext/pages/auth/LoginPage.tsx
+++ src/09-useContext/pages/auth/LoginPage.tsx
@@ -1,17 +1,40 @@
+import { UserContext } from "@/09-useContext/context/UserContext"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Link } from "react-router"
+import { useContext, useState } from "react"
+import { Link, useNavigate } from "react-router"
+import { toast } from "sonner"
 
 export const LoginPage = () => {
+
+    const { login } = useContext(UserContext);
+    const [userId, setUserId] = useState('');
+
+    const navigation = useNavigate();
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const result = login(+userId);
+
+        if (!result) {
+            toast.error('Usuario no encontrado');
+            return;
+        }
+
+        navigation('/profile');
+
+    }
     return (
         <div className="flex flex-col items-center min-h-screen">
             <h1 className="text-4xl font-bold">Inciar sesión</h1>
             <hr />
 
-            <form className="flex flex-col gap-2 my-10">
+            <form className="flex flex-col gap-2 my-10" onSubmit={handleSubmit}>
                 <Input
                     type="numver"
                     placeholder="Id del usuario"
+                    value={userId}
+                    onChange={e => setUserId(e.target.value)}
                 />
                 <Button type="submit">Login</Button>
             </form>
